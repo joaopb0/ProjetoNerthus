b68c072ca9eada7ee5ae54f3327595098653bd66
Joao Pedro Barbosa <joaobarbosa5337@gmail.com>
2025-10-26 01:12:37 -0300

    Criação da rota contato

--- routes/contato.js
+++ routes/contato.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('sobre');
+  res.render('contato');
 });
 
 module.exports = router;
